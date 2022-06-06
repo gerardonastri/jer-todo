13fe7a13559c7228394f3ea5c04cb822a8e0e5d7
gerardonastri <gerardonastri06@gmail.com>
2022-06-06 18:08:39 +0200

    better

--- todo/settings.py
+++ todo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,13 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 LOGIN_URL = 'login'
 
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [
+   # os.path.join(BASE_DIR,'static'),
+#]
 
 # production
-#STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
 
 # Default primary key field type

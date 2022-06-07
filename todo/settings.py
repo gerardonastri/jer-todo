1aff53a10116aa019cb1b6b596f136e4bc7c1335
gerardonastri <gerardonastri06@gmail.com>
2022-06-07 11:29:52 +0200

    better

--- todo/settings.py
+++ todo/settings.py
@@ -121,7 +121,7 @@ LOGIN_URL = 'login'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [
    # os.path.join(BASE_DIR,'static'),
 #]
